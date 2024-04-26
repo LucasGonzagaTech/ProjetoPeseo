01a7601b36bb7dc3f052a75271bc3798a126597e
isabellichrispiniano <132456611+isabellichrispiniano@users.noreply.github.com>
2024-04-26 09:34:17 -0300

    att isa

--- src/components/Recuperacao/Recuperacao.js
+++ src/components/Recuperacao/Recuperacao.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import './recuperacao.css';
- 
+import Header from '../Header/Header';
+import { Container } from 'react-bootstrap';
+import Seta from '../../img/setaRecuperacao.png';
+import Linha from '../../img/Separator.png';
+
 function Recuperacao() {
     return(
         <main>
-            <section>
-                <form className='recuperacao'>
-                     <h1>Recuperação da Conta</h1>
- 
+            <Header/>
+            <Container className='containerRecuperacao'>
+            <section className='recuperacao'>
+                <form className='form'>
+                     <h1 className='h1'>Recuperação da Conta</h1>
+            
+                     <img src={Linha}/>
+  
                      <div className='email'>
                         <label>E-mail</label>
-                        <input type='email' placeholder='Insira seu e-mail' required></input>
+                        <input className='borda2' type='email' placeholder='Insira seu e-mail' required></input>
                      </div>
  
-                     <button className='envioCodigo'>Enviar código para e-mail</button>
-                <p><a href='#'>voltar</a></p>
-                </form>
+               <button className='envioCodigo'>Enviar código para e-mail</button>
                    
+
+                <div className='voltar'>
+                <img src={Seta} alt='Seta' className='imagem'/>
+                <p className='voltar'><a href='#'>voltar</a></p>
+                </div>
+                </form>
             </section>
-        </main>
+            </Container> 
+        </main> 
     )
 }
  
