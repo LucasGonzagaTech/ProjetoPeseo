94d28b56c7a649048b932f28642023d855ba85eb
Victória Novaes <105504801+ViNovaes@users.noreply.github.com>
2024-04-26 08:59:46 -0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,18 +15,18 @@ const Header = () => {
             <Container>
             
                 <Navbar.Brand href="#home" className="me-auto">
-                <Link to={"/"}><img src={logo} alt="Logo" fluid /></Link>
+                    <Link to={"/"}><img src={logo} alt="Logo" fluid /></Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link href="" className="custom_nav_link">Como a Plataforma Funciona</Nav.Link>
-                        <Nav.Link href="#QuemSomos" className="custom_nav_link">Quem somos</Nav.Link>
+                        <Nav.Link href="sobre" className="custom_nav_link">Quem somos</Nav.Link>
                         <Nav.Link className="hide-on-mobile">|</Nav.Link>
-                        <Nav.Link href="#Entrar" className="custom_nav_link">Entrar</Nav.Link>
+                        <Nav.Link href="login" className="custom_nav_link">Entrar</Nav.Link>
                     </Nav>
                     <Button className="button-Accouny" variant="primary">
-                       <Link to={"/cadastro"}> <FontAwesomeIcon icon={faUser} /> Criar Conta </Link> 
+                        <Link to={"/cadastro"}> <FontAwesomeIcon icon={faUser} /> Criar Conta </Link> 
                     </Button>
                 </Navbar.Collapse>
             </Container>
